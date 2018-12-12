beaa75a2bd2a09efc161f5526613742c5740ef65
Soimo <jnr@bk.ru>
2018-12-12 16:04:02 +0300

    Commit

---
+++ Geometric/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Geometric")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Geometric")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("59b7bc53-4172-4cac-b76c-3f4e74a2cdd1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Geometric/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using GeometricFigures;
+using System;
+
+
+[TestFixture]
+public class GeometricFiguresTest
+{
+    [Test]
+    [TestCase(14, TestName = "CircleLenght")]
+    public void CircleLenght(int arg1)
+    {
+        Geometric.CirleTest ct = new Geometric.CirleTest();
+        ct.CircleLenghtTest(arg1);
+    }
+
+
+    [Test, TestCaseSource("CircleDividedCases")]
+    public void MultiplySource(int arg1)
+    {
+        Geometric.CirleTest ct = new Geometric.CirleTest();
+        ct.MultiplySourceTest(arg1);
+    }
+
+    static object[] CircleDividedCases =
+    {
+        new object[] {-6 },
+        new object[] { 0 },
+        new object[] { 5 }
+    };
+
+
+
+}
